1098effef2522439f11fe23ef894eb1b7c0c594e
Scott Ferguson <esfergus@gmail.com>
2015-08-22 10:56:28 -0500

    Lead controller added. SeedFile folder created and resume added.

---
+++ JobHunt.Web/JobHunt/Controllers/LeadsController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using JobHunt.Models;
+
+namespace JobHunt.Controllers
+{
+    public class LeadsController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/Leads
+        public IList<LeadCreateVM> GetLeads()
+        {
+            var query = db.Leads.Select(l => new LeadCreateVM() { JobTitle = l.Title, Company = l.Company.Name });
+            return query.ToList() ;
+        }
+
+        // GET: api/Leads/5
+        [ResponseType(typeof(Lead))]
+        public IHttpActionResult GetLead(int id)
+        {
+            Lead lead = db.Leads.Find(id);
+            if (lead == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(lead);
+        }
+
+        // PUT: api/Leads/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutLead(int id, Lead lead)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != lead.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(lead).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LeadExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Leads
+        [ResponseType(typeof(Lead))]
+        public IHttpActionResult PostLead(LeadCreateVM lead)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var newlead = new Lead() { Title = lead.JobTitle };
+            newlead.Company = new Company() { Name = lead.CompanyName };
+            db.Leads.Add(newlead);
+           
+            db.SaveChanges();
+
+            lead.LeadId = newlead.Id;
+            return Ok(lead);
+        }
+
+        // DELETE: api/Leads/5
+        [ResponseType(typeof(Lead))]
+        public IHttpActionResult DeleteLead(int id)
+        {
+            Lead lead = db.Leads.Find(id);
+            if (lead == null)
+            {
+                return NotFound();
+            }
+
+            db.Leads.Remove(lead);
+            db.SaveChanges();
+
+            return Ok(lead);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool LeadExists(int id)
+        {
+            return db.Leads.Count(e => e.Id == id) > 0;
+        }
+    }
+}
--- JobHunt.Web/JobHunt/Controllers/ValuesController.cs
+++ JobHunt.Web/JobHunt/Controllers/ValuesController.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 
 namespace JobHunt.Controllers
@@ -24,8 +25,10 @@ namespace JobHunt.Controllers
         }
 
         // POST api/values
-        public void Post([FromBody]string value)
+        [Route("ScottsRoute", Name = "ScottsRoute")]
+        public string Get(string resume)
         {
+            return "test";
         }
 
         // PUT api/values/5
@@ -36,25 +39,6 @@ namespace JobHunt.Controllers
         // DELETE api/values/5
         public void Delete(int id)
         {
-        }
-
-        //methods
-        public byte[] ReadFile()
-        {
-            using (MemoryStream ms = new MemoryStream())
-            using (FileStream file = new FileStream("file.bin", FileMode.Open, FileAccess.Read))
-            {
-                byte[] bytes = new byte[file.Length];
-                file.Read(bytes, 0, (int)file.Length);
-                ms.Write(bytes, 0, (int)file.Length);
-                return bytes;
-            }
-        }
-
-        public string WriteToFile(string filePath, byte[] bytes)
-        {
-            File.WriteAllBytes(filePath, bytes);
-            return filePath;
         }
     }
 }
