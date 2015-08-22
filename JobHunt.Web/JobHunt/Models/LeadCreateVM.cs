4a250ab818a2cff5364ce7240d689cbdc4c6fdba
Scott Ferguson <esfergus@gmail.com>
2015-08-22 13:35:34 -0500

    changed leadid to id on vm

--- JobHunt.Web/JobHunt/Models/LeadCreateVM.cs
+++ JobHunt.Web/JobHunt/Models/LeadCreateVM.cs
@@ -6,7 +6,7 @@ namespace JobHunt.Models
     {
         public string JobTitle { get; set; }
         public string CompanyName { get; set; }
-        public int LeadId { get; set; }
+        public int Id { get; set; }
         public DateTime PostTime { get; set; }
         public DateTime Deadline { get; set; }
     }
